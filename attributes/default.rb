9b17b9070ff64ffcdb0a16dd5e12431da67a5a11
nscendoni <scendoni@gmail.com>
2016-02-07 21:44:27 +0100

    fix

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,4 +3,15 @@ default['oud']['sysgroup'] = 'oracle'
 default['oud']['inventory_path'] = '/home/oracle'
 
 default['oud']['middleware_home'] = '/opt/oracle'
+default['oud']['jdk'] = 'http://192.168.101.1/jdk-7u79-linux-x64.rpm'
+default['oud']['installer_url'] = 'http://192.168.101.1/V75929-01.zip'
+
+default['oud']['instance_name'] = 'asinst_1'
+default['oud']['base_dn'] = 'dc=scendoni,dc=org'
+default['oud']['ldap_port'] = 1389
+default['oud']['admin_port'] = 4444
+default['oud']['root_user_dn'] = 'cn=Directory Manager'
+
+#TODO: Encrypt
+default['oud']['root_password'] = 'password'
 
